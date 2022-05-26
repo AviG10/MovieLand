0a84860ced9f08d9f39a7ae4fc5e037ff45949db
Avijit Ghosh <avijit@Avijits-MacBook-Air.local>
2022-05-26 16:07:11 +0530

    MovieLand First Commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import MovieCard from './MovieCard';
+import searchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=614c3044';
+
+const App = () => {
+
+  const [movies, setMovies] = useState([]);
+
+  const [searchTerm,setSearchTerm] = useState('')
+
+  const searchMovies = async (title)=>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  }
+
+
+  useEffect(()=>{
+    searchMovies('spiderman');
+  },[]);
+
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input placeholder='Search For Movies' value={searchTerm} onChange = {(e)=>setSearchTerm(e.target.value)} />
+        <img src={searchIcon} alt="Search" onClick={()=>searchMovies(searchTerm)}/>
+      </div>
+
+      {movies?.length > 0
+        ? (
+          <div className="container">
+            {movies.map((movie)=>(
+              <MovieCard movie = {movie}/>
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No Movies Found</h2>
+          </div>
+        )}
+
+    </div>
+  );
+}
+
+export default App;
